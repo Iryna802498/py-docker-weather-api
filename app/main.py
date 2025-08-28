ddcd5f438f418b9143875198726cfd39334bf856
Iryna Mishchenko <djrobentab@gmail.com>
2025-08-28 17:43:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,26 +2,33 @@ import requests
 import os
 
 
+BASE_URL = "http://api.weatherapi.com/v1/current.json?"
+CITY = "Paris"
+
+
 def get_weather() -> None:
     api_key = os.getenv("API_KEY")
     if not api_key:
         print("Error: API_KEY is not set.")
         return
     else:
-        url = "http://api.weatherapi.com/v1/current.json?"
-        filtering = "Paris"
-        result = requests.get(url + f"key={api_key}&" + f"q={filtering}")
-        if result.status_code == 200:
-            data = result.json()
-            city = data["location"]["name"]
-            country = data["location"]["country"]
-            day_time = data["location"]["localtime"]
-            temperatura = data["current"]["temp_c"]
-            type_weather = data["current"]["condition"]["text"]
-            print(f"{city}/{country} {day_time} "
-                  f"Weather: {temperatura} {type_weather}")
-        else:
-            print("Error: invalid request")
+        try:
+            result = requests.get(
+                BASE_URL + f"key={api_key}&" + f"q={CITY}",
+                timeout=5
+            )
+            result.raise_for_status()
+        except requests.RequestException as error:
+            print(f"Network error: {error}")
+            return
+        data = result.json()
+        city = data["location"]["name"]
+        country = data["location"]["country"]
+        day_time = data["location"]["localtime"]
+        temperatura = data["current"]["temp_c"]
+        type_weather = data["current"]["condition"]["text"]
+        print(f"{city}/{country} {day_time} "
+              f"Weather: {temperatura} {type_weather}")
 
 
 if __name__ == "__main__":
